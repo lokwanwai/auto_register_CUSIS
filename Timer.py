b8bbe3bf2a35237ddfdd552243faf30339601d09
Alex612 <69337548+lokwanwai@users.noreply.github.com>
2023-08-04 10:23:21 +0200

    making signal when targetTime met

---
+++ Timer.py
@@ -0,0 +1,39 @@
+import datetime as dt
+import time
+
+
+class Timer:
+    def __init__(self, targetTime: str, timeBuffer: float = 0.0003):
+        self.targetTime = dt.datetime.strptime(
+            dt.date.today().strftime("%Y-%m-%d") + targetTime, "%Y-%m-%d%H:%M:%S"
+        )
+        self.currentTime = dt.datetime.now()
+        self.timeBuffer = dt.timedelta(seconds=timeBuffer)
+        self.STATUS = False
+
+        print("Target time:", self.targetTime)
+        self.currentTimeReport()
+
+    def currentTimeReport(self):
+        print("Current time:", self.currentTime)
+
+    def run(self):
+        while True:
+            self.currentTime = dt.datetime.now()
+            if self.targetTime <= self.currentTime + self.timeBuffer:
+                self.currentTimeReport()
+                print("time is up")
+                self.STATUS = True
+                return True
+            else:
+                if self.targetTime - self.currentTime >= dt.timedelta(seconds=10):
+                    self.currentTimeReport()
+                    time.sleep(1)
+                else:
+                    self.currentTimeReport()
+
+
+if __name__ == "__main__":
+    target_time = "21:05:00"
+    timer = Timer(targetTime=target_time)
+    timer.run()
