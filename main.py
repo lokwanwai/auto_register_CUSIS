01bde4bff24385906c1ace7bd5af87cb56e7d85c
Alex612 <69337548+lokwanwai@users.noreply.github.com>
2023-08-05 01:29:17 +0200

    prevent browser quit right after clicking()

--- main.py
+++ main.py
@@ -2,29 +2,27 @@ import json
 import os
 from Timer import Timer
 from WebDriver import WebDriver
+import time
 
 
 class Controller:
     def __init__(self):
         self.get_config()
-        self.timer = Timer(self.register_time)
-        self.driver = WebDriver(self.login_email, self.login_password)
+        self.timer = Timer(self.REGISTER_TIME)
+        self.driver = WebDriver(self.LOGIN_EMAIL, self.LOGIN_PASSWORD)
 
     def get_config(self):
         cwd = os.getcwd()
         config_path = os.path.join(cwd, "config.json")
         with open(config_path) as config_file:
             config = json.load(config_file)
-            self.login_email = config["login_email"]
-            self.login_password = config["login_password"]
-            self.register_time = config["register_time"]
+            self.LOGIN_EMAIL = config["login_email"]
+            self.LOGIN_PASSWORD = config["login_password"]
+            self.REGISTER_TIME = config["register_time"]
 
     def set_register_mode(self):
         welcome = """
         =======================================
-        your login in this CUSIS successfully
-        please enter the enroll section 
-
         ### input your option after selecting of your choices
         ### (v)=validate
         ### (e)=enroll
@@ -32,7 +30,7 @@ class Controller:
         """
         print(welcome)
         self.register_mode = input("input: ")
-        while self.register_mode is not "v" or "e":
+        while (self.register_mode != "v") and (self.register_mode != "e"):
             print("please input the correct option")
             self.register_mode = input("input: ")
 
@@ -43,3 +41,4 @@ if __name__ == "__main__":
     controller.set_register_mode()
     controller.timer.run()
     controller.driver.register(controller.register_mode)
+    input("The programme is finished, enter any key to close the browser ")
